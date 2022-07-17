499d533790fffd827df31cef6e5b4b1d41d15fb4
DragonheartLX <dh@dragonheartlx.tk>
2022-07-17 18:09:41 +0800

    初始提交

---
+++ Sibilla/Sibilla/Sibilla.cpp
@@ -0,0 +1,34 @@
+#include "../MiraiCP/MiraiCP.hpp"
+using namespace MiraiCP;
+
+const PluginConfig CPPPlugin::config{
+        "com.DragonheartLX.Sibilla",            // 插件id
+        "SibillaBot",                           // 插件名称
+        "0.0.0alpha",                                // 插件版本
+        "DragonheartLX",                        // 插件作者
+        "A QQ Bot"                              // 可选：插件描述
+        //__DATE__ __TIME__                       // 可选：日期
+};
+
+// 插件实例
+class Main : public CPPPlugin {
+public:
+    // 配置插件信息
+    Main() : CPPPlugin() {}
+    ~Main() override = default;
+
+    // 入口函数
+    void onEnable() override {
+        // 请在此处监听
+    }
+
+    // 退出函数
+    void onDisable() override {
+        /*插件结束前执行*/
+    }
+};
+
+// 绑定当前插件实例
+void MiraiCP::enrollPlugin() {
+    MiraiCP::enrollPlugin(new Main);
+}
